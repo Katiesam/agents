baa299fb65ed624349a6dfc086f189232071a524
katherinehotexas <60495993+Katiesam@users.noreply.github.com>
2023-07-31 08:19:06 -0700

    Original sheet format works

--- agents.py
+++ agents.py
@@ -1,44 +1,41 @@
 import pandas as pd
 import numpy as np
-import pprint
-from collections import OrderedDict
+import datetime
+# import pprint
+# from collections import OrderedDict
 
-# class Agent:
-#     def __init__(self, name, agent_name, age, status):
-#         self.name = name
-#         self.agent_name = agent_name
-#         self.age = age
-#         self.status = status
-
-# agents = {}
 
 # Place Agents.csv into a DataFrame. Prompt for command.
-agents = pd.read_csv('Agents.csv', index_col='Name')
-print("Welcome. \nAvailable commands: add, remove, list, stats, in, out, update, shape, exit")
-print(agents.to_string())
+agents = pd.read_csv('Agents.csv')
+agents.index += 1
+print("Welcome. \nAvailable commands: add, remove, list, stats, in, out, shape, exit")
+print(agents)
 
 def add():
     name = str(input("Enter name: "))
     agent_name = str(input("Enter agent name: "))
     age = str(input("Enter age: "))
     status = False
-    row_df = pd.DataFrame([[name, agent_name, age, status]], columns=['Name', 'AgentName', 'Age', 'Status'])
-    agents.append(row_df, inplace=True)
-    update()
+    agents.loc[len(agents)+1] = [name, agent_name, age, status]
     print("Welcome " + name + ", a.k.a Agent " + agent_name + " (Age " + age + ")")
+    update()
 
 def remove():
     name = input("Enter name: ")
-    agents.drop(agents[name], inplace=True)
+    global agents
+    agents = agents[agents['Name'] != name]
     print(name + " has been removed.")
     update()
 
 def stats():
-    print("Stats: ")
     print("Number of agents: " + str(len(agents)))
-    p = 0
+    p = (agents['Status'] == True).sum()
     print("Agents Present: " + str(p))
     print("Agents Absent: " + str(len(agents) - p))
+    print("Youngest Agent: ")
+    print("Oldest Agent: ")
+    print("Average Agent Age: ")
+
 
 def in_agent():
     name = str(input("Enter name: "))
@@ -52,12 +49,13 @@ def out_agent():
     print("Agent " + name + " has departed.")
     update()
 
+def clear():
+    agents['Status'] = False
+    print("See y'all tomorrow!")
+    update()
+
 def update():
     agents.to_csv('Agents.csv', index=False)
-
-
-# a change
-# another change
 
 while True:
     command = input("\nEnter command: ")
@@ -73,8 +71,8 @@ while True:
         in_agent()
     elif command == "out":
         out_agent()
-    elif command == "update":
-        update()
+    elif command == "clear":
+        clear()
     elif command == "shape":
         print(agents.shape)
     elif command == "exit":
